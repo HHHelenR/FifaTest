67562694349322db02fa5526beeda3661c102958
helen <rqhhelen@gmail.com>
2022-12-04 13:22:21 -0500

    2nd commit -added main class

--- src/com/qa/fifa/Main.java
+++ src/com/qa/fifa/Main.java
@@ -3,8 +3,10 @@ package com.qa.fifa;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("I added a new class in this project");
 
+		
 	}
 
 }
